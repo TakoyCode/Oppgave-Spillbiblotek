c0a4f4c9fb5f787315028413105debc184bd3999
TakoyCode <129565940+TakoyCode@users.noreply.github.com>
2024-03-13 21:39:47 +0100

    Made addGameView Prettier

--- addGameView.js
+++ addGameView.js
@@ -1,41 +1,39 @@
-// addGameView();
+//addGameView();
 function addGameView() {
     const app = document.getElementById('app');
     let html = ``;
 
     html += /*HTML*/ `
-    <div>
-        <div>    
-            <div>Game Title:</div>
-            <input type="text" oninput="gameTitleInput = this.value" value = "${gameTitleInput}"/>
-        </div>
-        <br/>
-        
-        <div>
-            <div>Console:</div>
-            <select onchange="gameConsoleInput = this.value;">
-                <option>${gameConsoleInput}</option>
-                <option>Nintedo 64</option>
-                <option>Nintendo Entertainment System</option>
-                <option>Game Cube</option>
-            </select>
-        </div>
-        <br/>
+    <div class="addGameViewContainer">
+        <div class="addGameViewBackground">
+            <button class="addGameViewExitButton" onclick="mainView()">x</button> 
 
-        <div>
-            <div>Release Year:</div>
-            <input type="number" oninput="gameReleaseYearInput = this.value" value = "${gameReleaseYearInput}"/>
-        </div>
-        <br/>
-        
-        <div>
-         <div>Game Cover Art Link:</div>
-         <input type="text" oninput="gameImageInput = this.value" value = "${gameImageInput}"/>
-        </div>
-        <br/>
-        
-        <button onclick="addNewGame()">Legg til Spill</button>
-        <button onclick="mainView()">Tilbake</button>
+            <div class="addGameViewInputDiv">
+                <div class="addGameViewInputText">
+                    <div>Game Title:</div>
+                    <input type="text" oninput="gameTitleInput = this.value" value = "${gameTitleInput}"/>
+                </div>
+                <div class="addGameViewInputText">
+                    <div>Console:</div>
+                    <select onchange="gameConsoleInput = this.value;">
+                        <option>${gameConsoleInput}</option>
+                        <option>Nintedo 64</option>
+                        <option>Nintendo Entertainment System</option>
+                        <option>Game Cube</option>
+                    </select>
+                </div>
+                <div class="addGameViewInputText">
+                    <div>Release Year:</div>
+                    <input type="number" oninput="gameReleaseYearInput = this.value" value = "${gameReleaseYearInput}"/>
+                </div>
+                <div class="addGameViewInputText">
+                    <div>Game Cover Art Link:</div>
+                    <input type="text" oninput="gameImageInput = this.value" value = "${gameImageInput}"/>
+                </div>
+            </div>
+
+            <button class="addGameViewAddGameButton" onclick="addNewGame()">Legg til Spill</button>
+        </div>    
     </div>
 
     `;
